54f24c6edf7b173fcecfb2ad2964d1fea144d7f1
Romain Campech <romain.campech@gmail.com>
2020-09-19 17:57:01 +0100

    Revert changes

--- infrastructure/modules/video_stream_analysis/outputs.tf
+++ infrastructure/modules/video_stream_analysis/outputs.tf
@@ -19,14 +19,6 @@ resource "aws_cloudformation_stack" "_" {
             "Type": "String",
             "Value": "${data.aws_iot_endpoint.camera_credentials_provider_endpoint.endpoint_address}"
           }
-        },
-        "IoTCertificatePrivateKey": {
-          "Type": "AWS::SSM::Parameter",
-          "Properties": {
-            "Name": "iot-certificate-private-key",
-            "Type": "String",
-            "Value": "${aws_iot_certificate.camera_certificate.private_key}"
-          }
         }        
       },
       "Outputs": {
@@ -35,10 +27,7 @@ resource "aws_cloudformation_stack" "_" {
         },
         "IoTCredentialsProviderEndpoint": {
           "Value": "${data.aws_iot_endpoint.camera_credentials_provider_endpoint.endpoint_address}"
-        },
-        "IoTCertificatePrivateKey": {
-          "Value": "${aws_iot_certificate.camera_certificate.private_key}"
-        }        
+        }
       }
     }
   STACK
