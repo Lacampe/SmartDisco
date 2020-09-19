afafd1410a2b8a83503ea1471981c8af46f0835a
Romain Campech <romain.campech@gmail.com>
2020-09-19 18:41:10 +0100

    Remove IoT certificates and alias role

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = var.PROJECT
+    organization = "SmartDisco"
 
     workspaces {
-      name = var.ENV
+      name = "production"
     }
   }
 }
